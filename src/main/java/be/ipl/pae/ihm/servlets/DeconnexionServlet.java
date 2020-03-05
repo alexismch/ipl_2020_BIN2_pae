7fc1695f435d1054d74cf27da57aaa47d12f170f
Michiels Alexis <alexis.michiels@student.vinci.be>
2020-03-05 16:08:33 +0100

    feat: init. servlets

--- src/main/java/be/ipl/pae/ihm/servlets/DeconnexionServlet.java
+++ src/main/java/be/ipl/pae/ihm/servlets/DeconnexionServlet.java
@@ -1,5 +1,16 @@
 package be.ipl.pae.ihm.servlets;
 
-public class DeconnexionServlet {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class DeconnexionServlet extends HttpServlet {
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    super.doPost(req, resp);
+  }
 }
--- src/main/java/be/ipl/pae/main/Main.java
+++ src/main/java/be/ipl/pae/main/Main.java
@@ -1,9 +1,28 @@
 package be.ipl.pae.main;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello world !");
+    // server to listen on specific port 8080
+    Server server = new Server(80);
+    // create the object to configure the web application
+    WebAppContext context = new WebAppContext();
+
+    System.out.println(context.getContextPath());
+    context.setContextPath("/");
+
+    // this is to be able to make changes to .js files without having to restart everything
+    context.setInitParameter("cacheControl", "no-store,no-cache,must-revalidate");
+
+    // handling static content : create the shared folder of your web app
+    context.setResourceBase("public");
+
+    // provide the configuration object to the server
+    server.setHandler(context);
+    server.start();
   }
 
 }
