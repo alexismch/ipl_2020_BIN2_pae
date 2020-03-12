e579e77a4b25d21946badf3724220152f7de63fc
Michiels Alexis <alexis.michiels@student.vinci.be>
2020-03-12 14:44:09 +0100

    fix: test packages

---
+++ src/test/java/be/ipl/pae/dal/service/MockDalService.java
@@ -0,0 +1,14 @@
+package be.ipl.pae.dal.service;
+
+import be.ipl.pae.dal.services.DalService;
+import java.sql.PreparedStatement;
+
+public class MockDalService implements DalService {
+
+  @Override
+  public PreparedStatement getPreparedStatement(String requete) {
+
+    return null;
+  }
+
+}
