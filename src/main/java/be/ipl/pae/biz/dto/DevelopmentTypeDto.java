1b9c6c46774d348578b9fff1e64c66f7284c0041
Administrator <Administrator@Device>
2020-03-19 00:21:11 +0100

    feat: usecase impk

---
+++ src/main/java/be/ipl/pae/biz/dto/DevelopmentTypeDto.java
@@ -0,0 +1,13 @@
+package be.ipl.pae.biz.dto;
+
+public interface DevelopmentTypeDto {
+
+  int getIdType();
+
+  void setIdType(int idType);
+
+  String getTitle();
+
+  void setTitle(String title);
+  
+}
---
+++ src/main/java/be/ipl/pae/biz/objets/DevelopmentTypeImpl.java
@@ -0,0 +1,26 @@
+package be.ipl.pae.biz.objets;
+
+public class DevelopmentTypeImpl implements DevelopmentType {
+  
+  private int idType;
+  private String title;
+  public DevelopmentTypeImpl(int idType, String title) {
+    super();
+    this.idType = idType;
+    this.title = title;
+  }
+  public int getIdType() {
+    return idType;
+  }
+  public void setIdType(int idType) {
+    this.idType = idType;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  
+  
+}
