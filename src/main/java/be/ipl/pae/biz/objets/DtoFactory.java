8b73a2da024d73d1d751cbce3cb5b690ef63aa69
Administrator <Administrator@Device>
2020-03-18 23:58:09 +0100

    feat: correction

--- src/main/java/be/ipl/pae/biz/objets/DtoFactory.java
+++ src/main/java/be/ipl/pae/biz/objets/DtoFactory.java
@@ -1,11 +1,14 @@
 package be.ipl.pae.biz.objets;
 
+import be.ipl.pae.biz.dto.PhotoDto;
 import be.ipl.pae.biz.dto.QuoteDto;
 import be.ipl.pae.biz.dto.UserDto;
 import be.ipl.pae.biz.dto.UsersFilterDto;
 
 public interface DtoFactory {
 
+  PhotoDto getPhoto();
+  
   UserDto getUser();
   
   QuoteDto getQuote();
