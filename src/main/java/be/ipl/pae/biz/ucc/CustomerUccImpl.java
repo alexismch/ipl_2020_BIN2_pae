b56f7f07addda1263452d6537c54979f87ae396d
ilias <ilias@MSI.lan>
2020-03-19 02:24:47 +0100

    feat: properties

--- src/main/java/be/ipl/pae/biz/ucc/CustomerUccImpl.java
+++ src/main/java/be/ipl/pae/biz/ucc/CustomerUccImpl.java
@@ -1,5 +1,11 @@
 package be.ipl.pae.biz.ucc;
 
+import be.ipl.pae.dal.dao.CustomerDao;
+
+import be.ipl.pae.dependencies.Injected;
+
 public class CustomerUccImpl {
 
+  @Injected
+  private CustomerDao customerDao;
 }
