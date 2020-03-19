06abec2e06f20e0341fdff1d1dcc50c020d11d11
Michiels Alexis <alexis.michiels@student.vinci.be>
2020-03-19 16:26:10 +0100

    fix: checkstyle

--- src/main/java/be/ipl/pae/biz/objets/QuoteTypeImpl.java
+++ src/main/java/be/ipl/pae/biz/objets/QuoteTypeImpl.java
@@ -2,34 +2,34 @@ package be.ipl.pae.biz.objets;
 
 public class QuoteTypeImpl implements QuoteType {
 
- private String idquote;
- 
-private int idtype;
+  private String idquote;
+
+  private int idtype;
 
 
-public QuoteTypeImpl() {
-  super();
-}
+  public QuoteTypeImpl() {
+    super();
+  }
 
-public QuoteTypeImpl(String idquote, int idtype) {
-  super();
-  this.idquote = idquote;
-  this.idtype = idtype;
-}
+  public QuoteTypeImpl(String idquote, int idtype) {
+    super();
+    this.idquote = idquote;
+    this.idtype = idtype;
+  }
 
-public String getIdquote() {
-  return idquote;
-}
+  public String getIdquote() {
+    return idquote;
+  }
 
-public void setIdquote(String idquote) {
-  this.idquote = idquote;
-}
+  public void setIdquote(String idquote) {
+    this.idquote = idquote;
+  }
 
-public int getIdtype() {
-  return idtype;
-}
+  public int getIdtype() {
+    return idtype;
+  }
 
-public void setIdtype(int idtype) {
-  this.idtype = idtype;
-}
+  public void setIdtype(int idtype) {
+    this.idtype = idtype;
+  }
 }
