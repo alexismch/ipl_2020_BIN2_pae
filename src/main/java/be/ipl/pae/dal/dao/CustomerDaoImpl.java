178cd1f35a7ae5c5c0d10c9095373f0d72f06690
Administrator <Administrator@192.168.1.50>
2020-03-23 23:51:02 +0100

    feat: temp

--- src/main/java/be/ipl/pae/dal/dao/CustomerDaoImpl.java
+++ src/main/java/be/ipl/pae/dal/dao/CustomerDaoImpl.java
@@ -2,10 +2,17 @@ package be.ipl.pae.dal.dao;
 
 import be.ipl.pae.biz.dto.CustomerDto;
 import be.ipl.pae.biz.dto.CustomersFilterDto;
+import be.ipl.pae.biz.dto.UserDto;
 import be.ipl.pae.biz.objets.DtoFactory;
+import be.ipl.pae.biz.objets.UserStatus;
 import be.ipl.pae.dal.services.DalService;
+import be.ipl.pae.dal.util.DalUtils;
 import be.ipl.pae.dependencies.Injected;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDaoImpl implements CustomerDao {
@@ -13,7 +20,7 @@ public class CustomerDaoImpl implements CustomerDao {
   @Injected
   DalService dalService;
   @Injected
-  DtoFactory customerDto;
+  DtoFactory customerDtoFactory;
   @Override
   
   
@@ -25,10 +32,38 @@ public class CustomerDaoImpl implements CustomerDao {
     if(customersFilterDto==null) {
       query="SELECT * FROM mystherbe.customers";
     }else {
-      query ="SELECT * FROM mystherbe.customers WHERE (? IS NULL OR lastname LIKE ?)"
-          + " AND (? IS NULL OR city LIKE ?)";
+      query ="SELECT * FROM mystherbe.users WHERE lastname LIKE ? AND city LIKE ? AND postal_code LIKE ?";
     }
+    
+    PreparedStatement ps = dalService.getPreparedStatement(query);
+    if(customersFilterDto!=null) {
+      String name = DalUtils.escapeSpecialLikeChar(customersFilterDto.getName());
+      String city = DalUtils.escapeSpecialLikeChar(customersFilterDto.getCity());
+      int postalCode = customersFilterDto.getPostalCode();
+    }
+    
+
     
     return null;
   }
+  
+  /** Return list of customers created from the request. If only one customer is required you can call {@link
+    * List#get} with the index 0.
+    *
+    * @param ps The request that will be executed
+    * @return A list of CustomerDto created form the database
+    * @throws SQLException if an SQL error occurred
+    */
+   private List<CustomerDto> getUsersViaPs(PreparedStatement ps) throws SQLException {
+     List<CustomerDto> customers = new ArrayList<>();
+     try (ResultSet resultSet = ps.executeQuery()) {
+       while (resultSet.next()) {
+         CustomerDto customerDto = customerDtoFactory.getCustomer();
+         customers.add(customerDto);
+       }
+     }
+     ps.close();
+
+     return customers;
+   }
 }
