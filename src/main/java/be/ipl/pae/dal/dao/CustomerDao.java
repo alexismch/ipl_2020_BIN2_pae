599235472fd51fd2e40b99b33961c25f159821f7
ilias <ilias@MSI.lan>
2020-03-23 23:46:48 +0100

    feat: insertCustomer

--- src/main/java/be/ipl/pae/dal/dao/CustomerDao.java
+++ src/main/java/be/ipl/pae/dal/dao/CustomerDao.java
@@ -1,5 +1,16 @@
 package be.ipl.pae.dal.dao;
 
+import be.ipl.pae.biz.dto.CustomerDto;
+import be.ipl.pae.exceptions.FatalException;
+
 public interface CustomerDao {
 
+  /**
+   * insert a new Customer in the database.
+   *
+   * @param customer the user that you need to insert
+   * @return a CustomerDto if he is insert or null if he's not
+   * @throws FatalException if you have an error with the db
+   */
+  public CustomerDto insertCustomer(CustomerDto customer) throws FatalException;
 }
