fa6e2b4892c2815e543f9adc84b836c6c3dcd39d
soufiane <soufiane.hajmhand@student.vinci.be>
2020-04-02 20:56:40 +0100

    fix:jenkins

--- src/test/java/be/ipl/pae/main/QuoteUccImplTest.java
+++ src/test/java/be/ipl/pae/main/QuoteUccImplTest.java
@@ -1,12 +1,12 @@
 package be.ipl.pae.main;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import be.ipl.pae.biz.dto.QuoteDto;
 import be.ipl.pae.biz.objets.DtoFactory;
