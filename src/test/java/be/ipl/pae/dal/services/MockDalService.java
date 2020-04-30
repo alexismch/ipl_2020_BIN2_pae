889347dc7a7fcf920efa2ca7a496d8f9a7d6e55f
Mathieu COSYNS <mathieu.cosyns@student.vinci.be>
2020-05-01 00:14:53 +0200

    Fix: various bug fixes

--- src/test/java/be/ipl/pae/dal/services/MockDalService.java
+++ src/test/java/be/ipl/pae/dal/services/MockDalService.java
@@ -1,31 +1,31 @@
 package be.ipl.pae.dal.services;
 
-import be.ipl.pae.exceptions.DalException;
+import be.ipl.pae.exceptions.FatalException;
 
 public class MockDalService implements DalServiceTransaction {
 
   private boolean transactionStarted;
 
   @Override
-  public void startTransaction() throws DalException {
+  public void startTransaction() throws FatalException {
     if (this.transactionStarted) {
-      throw new DalException("Transaction already started");
+      throw new FatalException("Transaction already started");
     }
     this.transactionStarted = true;
   }
 
   @Override
-  public void commitTransaction() throws DalException {
+  public void commitTransaction() throws FatalException {
     if (!this.transactionStarted) {
-      throw new DalException("Transaction not started");
+      throw new FatalException("Transaction not started");
     }
     this.transactionStarted = false;
   }
 
   @Override
-  public void rollbackTransaction() throws DalException {
+  public void rollbackTransaction() throws FatalException {
     if (!this.transactionStarted) {
-      throw new DalException("Transaction not started");
+      throw new FatalException("Transaction not started");
     }
     this.transactionStarted = false;
   }
