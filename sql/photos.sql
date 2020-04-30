13fa3b0b9b343e5cd2418439686931bcf89f8d4c
Michiels Alexis <alexis.michiels@student.vinci.be>
2020-04-30 23:16:09 +0200

    fix: photoX.sql files

--- sql/photos.sql
+++ sql/photos.sql
@@ -1,11 +1,11 @@
 UPDATE mystherbe.quotes
-SET id_photo = 3
+SET id_photo = 5
 WHERE id_quote = '1';
 
 UPDATE mystherbe.quotes
-SET id_photo = 12
+SET id_photo = 13
 WHERE id_quote = '2';
 
 UPDATE mystherbe.quotes
-SET id_photo = 21
+SET id_photo = 19
 WHERE id_quote = '6';
