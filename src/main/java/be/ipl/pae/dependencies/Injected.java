f2f63476795fee302c8b14c779d2cb1058dac357
Mathieu COSYNS <mathieu.cosyns@student.vinci.be>
2020-05-01 16:11:30 +0200

    Fix: injection dependence dans DalServiceImpl.java

--- src/main/java/be/ipl/pae/dependencies/Injected.java
+++ src/main/java/be/ipl/pae/dependencies/Injected.java
@@ -1,9 +1,15 @@
 package be.ipl.pae.dependencies;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Injected {
 
+  String DEFAULT = "";
+
+  String value() default DEFAULT;
 }
