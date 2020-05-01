89aa9f738c1f9fe54da08478d19691be0e8404a8
Mathieu COSYNS <mathieu.cosyns@student.vinci.be>
2020-05-01 16:17:45 +0200

    Fix: checkstyle

--- src/main/java/be/ipl/pae/dependencies/Injected.java
+++ src/main/java/be/ipl/pae/dependencies/Injected.java
@@ -11,5 +11,11 @@ public @interface Injected {
 
   String DEFAULT = "";
 
+  /**
+   * If set to something different than {@link Injected#DEFAULT}, {@link InjectionService} will
+   * search for the property specified instead of the full class name
+   *
+   * @return the name of the property
+   */
   String value() default DEFAULT;
 }
