98aa65c91dd8c15d7d4becdd971c117f48091604
soufiane <soufiane.hajmhand@student.vinci.be>
2020-05-01 20:28:39 +0100

    fix: jenkins

--- src/test/java/be/ipl/pae/biz/ucc/DevelopmentTypeUccImplTest.java
+++ src/test/java/be/ipl/pae/biz/ucc/DevelopmentTypeUccImplTest.java
@@ -1,7 +1,7 @@
 package be.ipl.pae.biz.ucc;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import be.ipl.pae.biz.dto.DevelopmentTypeDto;
 import be.ipl.pae.biz.objets.DtoFactory;
